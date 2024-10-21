242d48a12847285959705005f9c6ec870b8361a7
Hagan <hagannn@mail.uc.edu>
2024-10-21 11:23:33 -0400

    Done
    Done trying to make a 'pretty' table

--- src/CtoFTableDisplay.java
+++ src/CtoFTableDisplay.java
@@ -4,33 +4,15 @@ public class CtoFTableDisplay {
     }
 
     public static void main(String[] args) {
-        int padding = 20;
-        System.out.print("┌");
-        for (int i = 0; i < padding; i++) {
-            System.out.print("─");
-        }
-        System.out.println("┐");
-        System.out.print("│");
 
-        for (int i = 0; i < padding/4-1; i++) {
-            System.out.print(" ");
-        }
-        System.out.print("Cel");
-        for (int i = 0; i < padding/4-1; i++) {
-            System.out.print(" ");
-        }
-        for (int i = 0; i < padding/4-1; i++) {
-            System.out.print(" ");
-        }
-        System.out.print("Feh");
-        for (int i = 0; i < padding/4-1; i++) {
-            System.out.print(" ");
-        }
+        System.out.print("    Cel");
+
+        System.out.print("        Feh");
         System.out.println();
 
         //System.out.println("│  Cel   │  Feh  │");
         for (double i = -100; i < 101; i++) {
-            System.out.printf("│ %3.2f │ %3.2f │", i, CtoF(i));
+            System.out.printf("│ %06.2f │ %06.2f │", i, CtoF(i));
             System.out.println();
         }
     }
